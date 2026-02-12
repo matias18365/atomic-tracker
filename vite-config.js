467166b80c8437d8110d14160d39209e5cdba369
matias18365 <matias1805ga@gmail.com>
2026-02-11 21:24:03 -0400

    vite changes

--- vite-config.js
+++ vite-config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/atomic-tracker/', // Debe coincidir EXACTAMENTE con el nombre de tu repo en GitHub
+  base: '/atomic-tracker/', // IMPORTANTE: Las barras inclinadas son obligatorias
 })
